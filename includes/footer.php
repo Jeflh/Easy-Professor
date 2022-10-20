efff9a8c6ce536a3cbfe4daf86464fa08c11df78
Juan Emmanuel Fernández de Lara Hernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2022-10-20 13:45:29 -0500

    Fijar footer al fin de la página.

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
-<footer class="bg-primary">
-    <p class="text-center text-light">Todos los derechos reservados <?php echo date('Y'); ?> &copy;</p>
+<footer class="bg-primary w-100" style="position: absolute; bottom: 0;">
+    <p class="text-center text-light mt-3">Todos los derechos reservados <?php echo date('Y'); ?> &copy;</p>
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
