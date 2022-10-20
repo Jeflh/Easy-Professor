7a8afb9425d4294dbd6f9bf2d55987af0e12f4e7
Juan Emmanuel Fernández de Lara Hernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2022-10-19 19:50:08 -0500

    Añadí boostrap

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,10 @@
 <footer class="footer seccion">
     <p class="copyright">Todos los derechos reservados <?php echo date('Y'); ?> &copy;</p>
   </footer>
-  
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+</script>  
+
 </body>
 </html>
--- includes/head.php
+++ includes/head.php
@@ -5,5 +5,5 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="/src/css/normalize.css">
-  <link rel="stylesheet" href="/src/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/src/css/bootstrap.css">
   <link rel="stylesheet" href="/src/css/styles.css">
