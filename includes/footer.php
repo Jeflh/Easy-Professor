2317e203c6f954022d068de3074072c45187a822
Juan Emmanuel Fernández de Lara Hernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2022-10-20 10:56:19 -0500

    Ajustes de posición y color

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,6 @@
-<footer class="footer seccion">
-    <p class="copyright">Todos los derechos reservados <?php echo date('Y'); ?> &copy;</p>
-  </footer>
-
+<footer class="bg-primary">
+    <p class="text-center text-light">Todos los derechos reservados <?php echo date('Y'); ?> &copy;</p>
+</footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
 </script>  
--- includes/header.php
+++ includes/header.php
@@ -1,20 +1,25 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Easy Professor</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarColor01">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Panel de control
-            <span class="visually-hidden">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Ajustes</a>
-        </li>
-      </ul>
+    <div class="d-flex">
+      <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul class="navbar-nav me-auto">
+
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Panel de control
+              <span class="visually-hidden">(current)</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link  href="#">Ajustes</a>
+          </li>
+
+          <li>
+            <a class="btn btn-outline-light ms-2" href="#">Cerrar sesión</a>
+          </li>
+        </ul>     
+      </div>
     </div>
   </div>
 </nav>
