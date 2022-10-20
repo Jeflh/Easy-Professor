6c63d25599cc3664b7a2d09a965641e5fb8c465a
Juan Emmanuel Fernández de Lara Hernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2022-10-20 10:56:51 -0500

    Creación de tarjetas y posicionamiento.

--- views/V_verPanel.php
+++ views/V_verPanel.php
@@ -6,6 +6,37 @@
 <body>
   <?php require('includes/header.php'); ?>
 
+  <div class="container">
+    <div class="d-flex justify-content-center">
+      <div class="col card text-white bg-primary mt-5 mb-3 me-3" style="max-width: 20rem;">
+        <div class="card-header ">
+          <h4 class="">Lista de asistencia</h4>
+        </div>
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+
+      <div class="col card text-white bg-primary mt-5 mb-3 me-3" style="max-width: 20rem;">
+        <div class="card-header ">
+          <h4 class="">Asignaturas</h4>
+        </div>
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+
+      <div class="col card text-white bg-primary mt-5 mb-3 me-3" style="max-width: 20rem;">
+        <div class="card-header ">
+          <h4 class="">Reporte</h4>
+        </div>
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
 <?php 
   require('includes/footer.php');
 ?>
