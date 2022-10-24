131006537d2f8e2b3fc470f4bd718c72728d4c38
Juan Emmanuel Fernández de Lara Hernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2022-10-23 22:45:42 -0500

    Ajustes en el footer, siempre abajo.

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,13 @@
-<footer class="bg-primary w-100" style="position: absolute; bottom: 0;">
-    <p class="text-center text-light mt-3">Todos los derechos reservados <?php echo date('Y'); ?> &copy;</p>
+<footer class="bg-primary text-center text-lg-start">
+
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);"> 
+    <a class="text-light" href="#"></a>
+    <p class="text-light">Todos los derechos reservados - Easy Professor <?php echo date('Y'); ?> &copy;</p>
+  </div>
 </footer>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+
+<script src="/src/js/bootstrap.bundle.min.js">
 </script>  
 
 </body>
