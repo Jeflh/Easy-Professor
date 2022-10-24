75bb7659dbb50f7b6892423c5a351b891fc91a55
Juan Emmanuel Fernández de Lara Hernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2022-10-23 22:46:44 -0500

    Cambios en nombre de archivos.

---
+++ views/inicio.php
@@ -0,0 +1,17 @@
+<?php 
+  require_once('includes/head.php');
+  require_once('includes/noLoggedNav.php'); 
+?>
+
+<main>
+
+  <div class="container">
+    <h1 class="text-center mt-2">Bienvenido a Easy Professor</h1>
+    <p> En construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción, en construcción</p>
+  </div>
+
+</main>
+
+<?php 
+  require_once('includes/footer.php');
+?>
