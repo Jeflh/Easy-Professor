4d4c7e7716fcbea3d489d78db414db967692e5f3
Juan Emmanuel Fernández de Lara Hernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2022-10-24 23:18:49 -0500

    Ajuste de identación.

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <?php
-	
-	require_once "config/config.php";
-	require_once "core/routes.php";
-	require_once "config/db.php";
-	require_once "controllers/Inicio.php";
-	
-	if(isset($_GET['c'])){
-		
-		$controlador = cargarControlador($_GET['c']);
-		
-		if(isset($_GET['a'])){
-			if(isset($_GET['id'])){
-				cargarAccion($controlador, $_GET['a'], $_GET['id']);
-				} else {
-				cargarAccion($controlador, $_GET['a']);
-			}
-			} else {
+
+require_once "config/config.php";
+require_once "core/routes.php";
+require_once "config/db.php";
+require_once "controllers/Inicio.php";
+
+if (isset($_GET['c'])) {
+
+	$controlador = cargarControlador($_GET['c']);
+
+	if (isset($_GET['a'])) {
+		if (isset($_GET['id'])) {
+			cargarAccion($controlador, $_GET['a'], $_GET['id']);
+		} else {
+			cargarAccion($controlador, $_GET['a']);
+		}
+	} else {
 			cargarAccion($controlador, MAIN_ACTION);
 		}
-		
-		} else {
-		
-		$controlador = cargarControlador(MAIN_CONTROLLER);
-		$accionTmp = MAIN_ACTION;
-		$controlador->$accionTmp();
-	}
+} else {
+
+	$controlador = cargarControlador(MAIN_CONTROLLER);
+	$accionTmp = MAIN_ACTION;
+	$controlador->$accionTmp();
+}
+
 ?>
