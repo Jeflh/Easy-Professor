2b51e95e4316580276aba9c40d28749546dc8e6f
Juan Emmanuel Fernández de Lara Hernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2022-10-26 17:45:58 -0500

    Se añadieron los atributos de la clase.

--- models/M_usuario.php
+++ models/M_usuario.php
@@ -4,34 +4,53 @@ class UsuariosModel{
   private $db;
   private $usuario;
 
+  private $nombre;
+  private $apellido;
+  private $correo;
+  private $password;
+  private $grado;
+
 
   public function __construct(){
     $this->db = Conectar::conexion();
-    $this->usuario;
   }
 
   public function getUsuario(){ 
     return $this->usuario;
   }
 
-  public function insertarUsuario($nombre, $apellido, $correo, $password, $grado){
-    $nombre = mysqli_real_escape_string($this->db, $_POST['nombre']);
-    $apellido = mysqli_real_escape_string($this->db, $_POST['apellido']);
-    $correo = mysqli_real_escape_string($this->db, $_POST['correo']);
-    $password = mysqli_real_escape_string($this->db, $_POST['password']);
-    $grado = mysqli_real_escape_string($this->db, $_POST['grado']);
+  public function insertarUsuario(){
+    $this->nombre = mysqli_real_escape_string($this->db, $_POST['nombre']);
+    $this->apellido = mysqli_real_escape_string($this->db, $_POST['apellido']);
+    $this->correo = mysqli_real_escape_string($this->db, $_POST['correo']);
+    $this->password = mysqli_real_escape_string($this->db, $_POST['password']);
+    $this->grado = mysqli_real_escape_string($this->db, $_POST['grado']);
 
-    $passwordHash = password_hash($password, PASSWORD_BCRYPT);
+    $passwordHash = password_hash($this->password, PASSWORD_BCRYPT);
 
-    $query = $this->db->query("INSERT INTO usuarios (nombre, apellido, correo, password, grado) VALUES ('$nombre', '$apellido', '$correo', '$passwordHash', '$grado')");
+    $query = $this->db->query("INSERT INTO usuarios (nombre, apellido, correo, password, grado) VALUES ('$this->nombre', '$this->apellido', '$this->correo', '$passwordHash', '$this->grado')");
   }
 
-  public function consultarUsuario($correo, $password){
-    $correo = mysqli_real_escape_string($this->db, $_POST['correo']);
+  public function consultarUsuario(){
+    $this->correo = mysqli_real_escape_string($this->db, $_POST['correo']);
 
-    $query = $this->db->query("SELECT * FROM usuarios WHERE correo = '$correo'"); 
+    $query = $this->db->query("SELECT * FROM usuarios WHERE correo = '$this->correo'"); 
     
     $this->usuario = $query->fetch_assoc();
+  }
+
+  public function validarCorreo(){
+    $this->correo = mysqli_real_escape_string($this->db, $_POST['correo']);
+
+    $query = $this->db->query("SELECT * FROM usuarios WHERE correo = '$this->correo'"); 
+    
+    $this->usuario = $query->fetch_assoc();
+    
+    if($this->usuario){
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
