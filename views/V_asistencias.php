b96bee2b7ca4a9b07637b3a7f217b5ddf07389b5
Juan Emmanuel Fernández de Lara Hernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2022-10-27 15:19:26 -0500

    Ajuste de tamaño en titulos.

---
+++ views/V_asistencias.php
@@ -0,0 +1,75 @@
+<?php
+require_once('includes/head.php');
+require_once('includes/loggedNav.php');
+?>
+
+<main>
+  <p class="text-primary text-end me-3 mb-0 lead">
+    <?php echo $_SESSION['usuario']['nombre'] . ' ' . $_SESSION['usuario']['apellido'] ?>
+  </p>
+
+  <p class="text-info text-end me-3 mb-0 lead">
+    <?php date_default_timezone_set('America/Mexico_City');
+    echo date('d/m/Y'); ?>
+  </p>
+
+  <h1 class="text-primary text-center">Lista de asistencia</h1>
+
+  <div class="container d-flex justify-content-center">
+
+    <div class="col-5">
+      <table class="table table-striped table-bordered table-hover table-primary">
+        <thead>
+          <tr class="text-center">
+            <th class="col-1" scope="col">Nº</th>
+            <th class="col-2" scope="col">Apellido</th>
+            <th class="col-2" scope="col">Nombre</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php $i = 1;
+          foreach ($lista['alumnos'] as $alumno) { ?>
+            <tr class="table-light">
+              <th class="text-center" scope="row"><?php echo $i ?></th>
+              <td><?php echo $alumno['apellido']; ?></td>
+              <td><?php echo $alumno['nombre']; ?></td>
+            </tr>
+          <?php $i++;
+          } ?>
+
+        </tbody>
+      </table>
+    </div>
+
+    <div class="col-2">
+      <form data-bitwarden-watching="1" action="index.php?c=asistencia&a=guardar" method="POST">
+        <table class="table table-striped table-bordered table-hover table-primary">
+          <thead>
+            <tr class="text-center">
+              <th class="col-2" scope="col"> Asistencia</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach ($lista['alumnos'] as $alumno) { ?>
+              <tr class="table-light">
+                <td class="text-center">
+                  <div class="form-switch">
+                    <input class="form-check-input w-100" type="checkbox" name="<?php echo $alumno['id_alumno'] ?>" id="<?php echo $alumno['id_alumno'] ?>">
+                  </div>
+                </td>
+              </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+        <div class="d-grid col">
+          <button type="submit" class="btn btn-primary mb-2">Guardar asistencia</button>
+        </div>
+      </form>
+    </div>
+  </div>
+
+</main>
+
+<?php
+require_once('includes/footer.php');
+?>
--- views/V_registro.php
+++ views/V_registro.php
@@ -45,68 +45,70 @@ if (isset($_GET['e'])) {
 }
 ?>
 
-<div class="container d-flex justify-content-center mb-3">
-  <form data-bitwarden-watching="1" action="index.php?c=registro&a=validar" method="POST">
-    <fieldset>
-      <legend class="mt-4 text-center text-primary">
-        <h1><strong>Registro</strong></h1>
-      </legend>
-      <div class="form-group">
-        <label for="apellido" class="form-label mt-2">Nombre</label>
-        <input name="nombre" type="text" class="form-control" id="nombre" aria-describedby="nameHelp" placeholder="Ej. Emmanuel" autocomplete="off">
-      </div>
-      <div class="form-group">
-        <label for="apellido" class="form-label mt-2">Apellido</label>
-        <input name="apellido" type="text" class="form-control" id="apellido" aria-describedby="nameHelp" placeholder="Ej. Fernández" autocomplete="off">
-      </div>
-      <div class="form-group">
-        <label for="correo" class="form-label mt-2">Dirección de correo</label>
-        <input name="correo" type="email" class="form-control" id="correo" aria-describedby="emailHelp" placeholder="correo@ejemplo.com" autocomplete="off">
-        <small id="emailHelp" class="form-text text-muted">Nunca compartiremos su correo electrónico.</small>
-      </div>
-      <div class="form-group">
-        <label for="password" class="form-label mt-2">Contraseña</label>
-        <input name="password" type="password" class="form-control" id="password" placeholder="Escribe aquí tu contraseña" autocomplete="off">
-      </div>
-
-      <div class="form-group">
-        <label for="grado" class="form-label mt-4">Grado de tu grupo</label>
-        <select class="form-select" id="grado" name="grado">
-          <option>1er grado</option>
-          <option>2do grado</option>
-          <option>3er grado</option>
-          <option>4to grado</option>
-          <option>5to grado</option>
-          <option>6to grado</option>
-        </select>
-      </div>
-
-      <div class="form-check form-switch mt-4">
-        <input class="form-check-input" type="checkbox" id="novedades" checked="">
-        <label class="form-check-label">Recibir las ultimas novedades.</label>
-      </div>
-
-      <fieldset class="form-group">
-        <legend class="mt-3">Terminos y condiciones de uso</legend>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="privacidad" required>
-          <label class="form-check-label" for="flexCheckDefault">
-            He leído y estoy de acuerdo con la <a href="">politica de privacidad</a>.
-          </label>
+<main>
+  <div class="container d-flex justify-content-center mb-3">
+    <form data-bitwarden-watching="1" action="index.php?c=registro&a=validar" method="POST">
+      <fieldset>
+        <legend class="mt-4 text-center text-primary">
+          <h1><strong>Registro</strong></h1>
+        </legend>
+        <div class="form-group">
+          <label for="apellido" class="form-label mt-2">Nombre</label>
+          <input name="nombre" type="text" class="form-control" id="nombre" aria-describedby="nameHelp" placeholder="Ej. Emmanuel" autocomplete="off">
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="terminos" required>
-          <label class="form-check-label" for="flexCheckDefault">
-            Acepto los <a href="">terminos y condiciones de uso</a>.
-          </label>
+        <div class="form-group">
+          <label for="apellido" class="form-label mt-2">Apellido</label>
+          <input name="apellido" type="text" class="form-control" id="apellido" aria-describedby="nameHelp" placeholder="Ej. Fernández" autocomplete="off">
+        </div>
+        <div class="form-group">
+          <label for="correo" class="form-label mt-2">Dirección de correo</label>
+          <input name="correo" type="email" class="form-control" id="correo" aria-describedby="emailHelp" placeholder="correo@ejemplo.com" autocomplete="off">
+          <small id="emailHelp" class="form-text text-muted">Nunca compartiremos su correo electrónico.</small>
+        </div>
+        <div class="form-group">
+          <label for="password" class="form-label mt-2">Contraseña</label>
+          <input name="password" type="password" class="form-control" id="password" placeholder="Escribe aquí tu contraseña" autocomplete="off">
+        </div>
+
+        <div class="form-group">
+          <label for="grado" class="form-label mt-4">Grado de tu grupo</label>
+          <select class="form-select" id="grado" name="grado">
+            <option>1er grado</option>
+            <option>2do grado</option>
+            <option>3er grado</option>
+            <option>4to grado</option>
+            <option>5to grado</option>
+            <option>6to grado</option>
+          </select>
+        </div>
+
+        <div class="form-check form-switch mt-4">
+          <input class="form-check-input" type="checkbox" id="novedades" checked="">
+          <label class="form-check-label">Recibir las ultimas novedades.</label>
+        </div>
+
+        <fieldset class="form-group">
+          <legend class="mt-3">Terminos y condiciones de uso</legend>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="privacidad" required>
+            <label class="form-check-label" for="flexCheckDefault">
+              He leído y estoy de acuerdo con la <a href="">politica de privacidad</a>.
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="terminos" required>
+            <label class="form-check-label" for="flexCheckDefault">
+              Acepto los <a href="">terminos y condiciones de uso</a>.
+            </label>
+          </div>
+        </fieldset>
+        <div class="d-grid gap-2">
+          <button type="submit" class="btn btn-primary mt-3">Registrarse</button>
         </div>
       </fieldset>
-      <div class="d-grid gap-2">
-        <button type="submit" class="btn btn-primary mt-3">Registrarse</button>
-      </div>
-    </fieldset>
-  </form>
-</div>
+    </form>
+  </div>
+</main>
 
 <?php
 require_once('includes/footer.php');
