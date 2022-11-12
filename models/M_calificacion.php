557ee42b95f37d3d705af5cbbe4d6b35bd9171ff
Juan Emmanuel Fernández de Lara Hernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2022-11-12 15:03:22 -0600

    Se simplificó el código.

--- models/M_calificacion.php
+++ models/M_calificacion.php
@@ -20,19 +20,18 @@ class CalificacionModel{
   }
 
   public function setCal($calificacion){
-    $fecha = mysqli_real_escape_string($this->db, $calificacion['fecha_cal']);
-    $id_alumno = mysqli_real_escape_string($this->db, $calificacion['id_alumno']);
-    $actividad = mysqli_real_escape_string($this->db, $calificacion['actividad']);
-    $calificacion = mysqli_real_escape_string($this->db, $calificacion['calificacion']);
+    $fecha = $calificacion['fecha_cal'];
+    $id_alumno = $calificacion['id_alumno'];
+    $actividad = $calificacion['actividad'];
+    $calificacion = $calificacion['calificacion'];
 
     $query = $this->db->query("INSERT INTO calificacion (fecha_cal, id_alumno, id_actividad, calificacion) VALUES ('$fecha', '$id_alumno', '$actividad', '$calificacion')");
   }
 
   public function updateCal($calificacion){
-    $fecha = mysqli_real_escape_string($this->db, $calificacion['fecha_cal']);
-    $id_alumno = mysqli_real_escape_string($this->db, $calificacion['id_alumno']);
-    $actividad = mysqli_real_escape_string($this->db, $calificacion['actividad']);
-    $calificacion = mysqli_real_escape_string($this->db, $calificacion['calificacion']);
+    $id_alumno = $calificacion['id_alumno'];
+    $actividad = $calificacion['actividad'];
+    $calificacion = $calificacion['calificacion'];
 
     $query = $this->db->query("UPDATE calificacion SET calificacion = '$calificacion' WHERE id_alumno = '$id_alumno' AND id_actividad = '$actividad'");
   }
