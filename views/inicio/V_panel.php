d35fe6967673fefb7be6356825cd5b70b1e1ae6f
Emmanuel Fernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2022-11-25 13:28:04 -0600

    se agregó el manual de usuario

--- views/inicio/V_panel.php
+++ views/inicio/V_panel.php
@@ -82,7 +82,7 @@ require_once('includes/loggedNav.php');
 
 
   <div class="d-flex justify-content-end mt-5">
-    <a href="#" class="btn btn-primary me-3">
+    <a href="https://drive.google.com/file/d/1g8GIiNAKvVfcoSiEXJX_lnZ8Qpv9OcT4/view?usp=sharing" target="_blank" class="btn btn-primary me-3">
       <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-question-lg" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M4.475 5.458c-.284 0-.514-.237-.47-.517C4.28 3.24 5.576 2 7.825 2c2.25 0 3.767 1.36 3.767 3.215 0 1.344-.665 2.288-1.79 2.973-1.1.659-1.414 1.118-1.414 2.01v.03a.5.5 0 0 1-.5.5h-.77a.5.5 0 0 1-.5-.495l-.003-.2c-.043-1.221.477-2.001 1.645-2.712 1.03-.632 1.397-1.135 1.397-2.028 0-.979-.758-1.698-1.926-1.698-1.009 0-1.71.529-1.938 1.402-.066.254-.278.461-.54.461h-.777ZM7.496 14c.622 0 1.095-.474 1.095-1.09 0-.618-.473-1.092-1.095-1.092-.606 0-1.087.474-1.087 1.091S6.89 14 7.496 14Z" />
       </svg>
